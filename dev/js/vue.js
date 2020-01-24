2619ef91e89e33bfe802c6073420322b0a7be318
Sarah Proper <sarahproper18@gmail.com>
2020-01-24 12:55:34 -0700

    lunch code & comments

--- dev/js/vue.js
+++ dev/js/vue.js
@@ -1,18 +1,27 @@
+// const Vue = require('vue');
+// const axios = require('axios');
+
 new Vue({
-  el: '#vue-app',
+  // so we don't fight django
   delimiters: ['[[', ']]'],
-  data() {
-    return {
-      apiArticles: null,
-      isErrored: false,
-      isLoading: true
-    }
+  el: '#vue-app',
+  data: {
+    apiArticles: null,
+    isErrored: false,
+    isLoading: true
   },
-  mounted() {
+  /*
+  * on mount, run our axios request
+  * 
+  * on response, update our vue data obj with response data
+  * on error, log error and throw up error flag
+  * on end, turn off loading flag
+  */
+  mounted: function() {
     axios
       .get('/api/articles')
       .then(response => {
-      	console.log(response.data.results);
+        console.log(response.data.results);
         this.apiArticles = response.data.results;
       })
       .catch(error => {
@@ -20,5 +29,35 @@ new Vue({
         this.isErrored = true;
       })
       .finally(() => this.isLoading = false);
+  },
+  filters: {
+  	/**
+		 * Convert date
+		 *
+		 * takes in a date string and formats it to a readable date
+		 *
+		 * @param {string} takes in a date as a string
+		 * @return {string} 
+		 */
+    convertDate: function(dateToConvert) {
+      let formattedDate = new Date(Date.parse(dateToConvert));
+      dateToConvert = formattedDate.toLocaleString();
+      return dateToConvert;
+    },
+  },
+  methods: {
+  	/**
+		 * get the first background image and set it as background
+		 *
+		 * for article previews, only show the first image from images json data
+		 * then set that as a background image
+		 *
+		 * @param {obj} the article context passed from the template
+		 * @return {string} the background image text with url of image 
+		 */
+    makeFirstImageBackground: function(apiArticleContext) { 
+    	let url = apiArticleContext.images[0].url;
+      return `background-image: url('${url}')`; 
+    }
   }
 });
--- static/scripts.js
+++ static/scripts.js
@@ -1,15 +1,24 @@
 "use strict";
 
+// const Vue = require('vue');
+// const axios = require('axios');
 new Vue({
-  el: '#vue-app',
+  // so we don't fight django
   delimiters: ['[[', ']]'],
-  data: function data() {
-    return {
-      apiArticles: null,
-      isErrored: false,
-      isLoading: true
-    };
+  el: '#vue-app',
+  data: {
+    apiArticles: null,
+    isErrored: false,
+    isLoading: true
   },
+
+  /*
+  * on mount, run our axios request
+  * 
+  * on response, update our vue data obj with response data
+  * on error, log error and throw up error flag
+  * on end, turn off loading flag
+  */
   mounted: function mounted() {
     var _this = this;
 
@@ -22,5 +31,35 @@ new Vue({
     })["finally"](function () {
       return _this.isLoading = false;
     });
+  },
+  filters: {
+    /**
+    * Convert date
+    *
+    * takes in a date string and formats it to a readable date
+    *
+    * @param {string} takes in a date as a string
+    * @return {string} 
+    */
+    convertDate: function convertDate(dateToConvert) {
+      var formattedDate = new Date(Date.parse(dateToConvert));
+      dateToConvert = formattedDate.toLocaleString();
+      return dateToConvert;
+    }
+  },
+  methods: {
+    /**
+    * get the first background image and set it as background
+    *
+    * for article previews, only show the first image from images json data
+    * then set that as a background image
+    *
+    * @param {obj} the article context passed from the template
+    * @return {string} the background image text with url of image 
+    */
+    makeFirstImageBackground: function makeFirstImageBackground(apiArticleContext) {
+      var url = apiArticleContext.images[0].url;
+      return "background-image: url('".concat(url, "')");
+    }
   }
 });
